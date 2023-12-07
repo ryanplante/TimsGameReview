56096ee72a09de0a3fff0ef4da9bdb80aaa263f7
Dan <knight.daniel.r@gmail.com>
2023-12-07 15:18:11 -0500

    AddsGenreAndVideoGameControllers

---
+++ GameAPI/GameAPI/Controllers/VideoGameController.cs
@@ -0,0 +1,42 @@
+﻿using DataLibrary.Data;
+using DataLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GameAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoRatingController : ControllerBase
+    {
+        private readonly IVideoGameData _videoGameData;
+
+        public VideoRatingController(IVideoGameData videoGameData)
+        {
+            _videoGameData = videoGameData;
+        }
+
+        [HttpGet("GetGames")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<VideoGameModel> result = await _videoGameData.GetGames();
+
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+
+                return NotFound($"No games found.");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal server error");
+            }
+        }
+    }
+}
