ec54a31081a11ed7077bee1736faacb0603ebeef
Ryan Plante <rmplante@email.neit.edu>
2023-12-07 18:14:39 -0500

    add blazor page to test api

---
+++ BlazorReview/BlazorReview/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorReview.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string UName { get; set; }
+        public int PermissionLevel { get; set; }
+        public string Password { get; set; }
+    }
+
+}
